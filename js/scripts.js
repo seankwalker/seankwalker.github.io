1411ccc055f324ef35685ce1ea05c2d611b06beb
arithmetician1 <sean_walker@outlook.com>
2016-10-31 00:41:58 -0400

    Update bio, scripts. Add analytics.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#footer-wrapper").mouseover(function() {
+    $("#footer").mouseover(function() {
         $("#footer").fadeTo(100, 1);
     }).mouseout(function() {
         $("#footer").fadeTo(100, 0.20);
