dfca5f0be0869bfa879b16ad6fca37a70982c4ac
sean-walker <sean_walker@outlook.com>
2017-04-03 23:37:47 -0400

    Update: April 2017.

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#footer").mouseover(function() {
-        $("#footer").fadeTo(100, 1);
-    }).mouseout(function() {
-        $("#footer").fadeTo(100, 0.20);
-    });
-});
