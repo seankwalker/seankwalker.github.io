2d82f4c91d32bdbc1707e6b66c51030299af885d
Sean Walker <sean@seankwalker.com>
2020-04-11 17:46:15 -0700

    add in stubs for portfolio, resume sections

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,36 @@
 const TITLE_ID = "section-title";
 const BODY_ID = "section-body";
 
+const SECTION_ID_STUB = "section-selector-";
+
 // content for sections
+// CSS ID can be constructed from "section-selector-" + section title
 const CONTENT = [
   {
-    id: "section-selector-about",
     title: "about",
     body:
       "Bacon ipsum dolor amet rump fatback sirloin pork. Picanha tenderloin frankfurter tri-tip capicola turducken alcatra. Beef kevin shank, beef ribs doner ham hock prosciutto tongue venison chicken meatloaf filet mignon landjaeger. Corned beef chuck spare ribs kielbasa buffalo salami pastrami prosciutto venison cow beef chicken.",
   },
+  {
+    title: "portfolio",
+    body: "indev",
+  },
+  {
+    title: "resume",
+    body: "indev",
+  },
 ];
 
-const createClickHandlerForSection = ({ title, body, id }) => (event) => {
+const createClickHandlerForSection = ({ title, body }) => (event) => {
   const sectionIsSelected = $(`#${TITLE_ID}`).html() === title;
   const newTitle = sectionIsSelected ? "" : title;
   const newBody = sectionIsSelected ? "" : body;
 
   $(`#${TITLE_ID}`).html(newTitle);
   $(`#${BODY_ID}`).html(newBody);
-  $(`#${id}`).css("text-decoration", sectionIsSelected ? "none" : "underline");
+  $(`#${SECTION_ID_STUB}${title}`).css("text-decoration", sectionIsSelected ? "none" : "underline");
 };
 
-CONTENT.forEach((contentSpec) => $(`#${contentSpec.id}`).on("click", createClickHandlerForSection(contentSpec)));
+CONTENT.forEach((contentSpec) =>
+  $(`#${SECTION_ID_STUB}${contentSpec.title}`).on("click", createClickHandlerForSection(contentSpec))
+);
