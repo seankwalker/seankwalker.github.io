a7770fc784d597594ff01646450509ba0ff26d9c
Sean Walker <sean@seankwalker.com>
2020-04-22 17:23:41 -0700

    run Prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,62 @@
 "use strict";
 
-const TITLE_ID = "section-title";
-const BODY_ID = "section-body";
+const TITLE_ID = "#section-title";
+const BODY_ID = "#section-body";
+const NAV_ID = "#section-pages-nav";
 
-const SECTION_ID_STUB = "section-selector-";
+const SECTION_ID_STUB = "#section-selector-";
 
 // content for sections
 // CSS ID can be constructed from "section-selector-" + section title
 const CONTENT = [
-  {
-    title: "about",
-    body:
-      "Bacon ipsum dolor amet rump fatback sirloin pork. Picanha tenderloin frankfurter tri-tip capicola turducken alcatra. Beef kevin shank, beef ribs doner ham hock prosciutto tongue venison chicken meatloaf filet mignon landjaeger. Corned beef chuck spare ribs kielbasa buffalo salami pastrami prosciutto venison cow beef chicken.",
-  },
-  {
-    title: "portfolio",
-    body: "indev",
-  },
-  {
-    title: "resume",
-    body: "indev",
-  },
+    {
+        id: `${SECTION_ID_STUB}about`,
+        title: "about",
+        body:
+            "Bacon ipsum dolor amet rump fatback sirloin pork. Picanha tenderloin frankfurter tri-tip capicola turducken alcatra. Beef kevin shank, beef ribs doner ham hock prosciutto tongue venison chicken meatloaf filet mignon landjaeger. Corned beef chuck spare ribs kielbasa buffalo salami pastrami prosciutto venison cow beef chicken.",
+        multipage: false,
+    },
+    {
+        id: `${SECTION_ID_STUB}portfolio`,
+        title: "portfolio",
+        body:
+            "since june 2019, i've worked as a fullstack engineer at snackpass (yc w18). snackpass offers convenient, discounted food ordering (delivery, pickup, and dine-in) for customers and powerful analytics, marketing, and other business tools to our restaurant partners. i primarily focus on our partner-facing products, including a point-of-sale (pos) iOS app, an in-store self order kiosk, and various internal tools and dashboards.",
+        multipage: true,
+        pages: [{}],
+    },
+    {
+        id: `${SECTION_ID_STUB}resume`,
+        title: "resume",
+        body: "indev",
+        multipage: false,
+    },
 ];
 
 const updateSelectedDisplay = () =>
-  CONTENT.forEach(({ title, body }) => {
-    const sectionIsSelected = $(`#${TITLE_ID}`).html() === title;
-    $(`#${SECTION_ID_STUB}${title}`).css("text-decoration", sectionIsSelected ? "underline" : "none");
-  });
+    CONTENT.forEach(({ title, body }) => {
+        const sectionIsSelected = $(TITLE_ID).html() === title;
+        $(`${SECTION_ID_STUB}${title}`).css(
+            "text-decoration",
+            sectionIsSelected ? "underline" : "none"
+        );
+    });
 
-const createClickHandlerForSection = ({ title, body }) => (event) => {
-  const sectionIsSelected = $(`#${TITLE_ID}`).html() === title;
-  const newTitle = sectionIsSelected ? "" : title;
-  const newBody = sectionIsSelected ? "" : body;
+const createClickHandlerForSection = ({ title, body, multipage }) => (
+    event
+) => {
+    const sectionIsSelected = $(TITLE_ID).html() === title;
+    const newTitle = sectionIsSelected ? "" : title;
+    const newBody = sectionIsSelected ? "" : body;
+    const newMultipage = !sectionIsSelected && multipage;
 
-  $(`#${TITLE_ID}`).html(newTitle);
-  $(`#${BODY_ID}`).html(newBody);
+    $(TITLE_ID).html(newTitle);
+    $(BODY_ID).html(newBody);
 
-  updateSelectedDisplay();
+    newMultipage ? $(NAV_ID).show() : $(NAV_ID).hide();
+
+    updateSelectedDisplay();
 };
 
 CONTENT.forEach((contentSpec) =>
-  $(`#${SECTION_ID_STUB}${contentSpec.title}`).on("click", createClickHandlerForSection(contentSpec))
+    $(contentSpec.id).on("click", createClickHandlerForSection(contentSpec))
 );
